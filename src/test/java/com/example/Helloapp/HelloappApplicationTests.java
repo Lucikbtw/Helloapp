81238196630494ae4e639fa12837cfcda214e6fc
Lucikbtw <fouraj.4aj@gmail.com>
2025-04-18 20:06:22 +0300

    Initial commit

---
+++ src/test/java/com/example/Helloapp/HelloappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Helloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
